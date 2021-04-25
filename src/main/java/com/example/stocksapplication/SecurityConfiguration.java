c913b1d07b0eb155adbc31cc7b8741a1b857d3c8
Evan Mead <evan.c.mead@gmail.com>
2021-04-25 16:58:51 -0500

    user authentication created

--- src/main/java/com/example/stocksapplication/SecurityConfiguration.java
+++ src/main/java/com/example/stocksapplication/SecurityConfiguration.java
@@ -1,8 +1,54 @@
 package com.example.stocksapplication;
 
+import com.example.stocksapplication.services.UserDetailsLoader;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-public class SecurityConfiguration {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private final UserDetailsLoader userDetailsLoader;
 
+    public SecurityConfiguration(UserDetailsLoader userDetailsLoader) {
+        this.userDetailsLoader = userDetailsLoader;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authentication) throws Exception {
+        authentication.userDetailsService(userDetailsLoader).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//      DEFINE HOW TO LOGIN -> AND REDIRECTED AFTER LOGIN
+        http.formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/dashboard")
+                .permitAll()
+                // DEFINE HOW TO LOGOUT
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/")
+                // DEFINE VIEWS WHERE A USER DOESN'T NEED TO BE LOGGED IN
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/sign-up")
+                .permitAll()
+                // DEFINE VIEW WHERE A USER MUST BE LOGGED IN
+                .and()
+                .authorizeRequests()
+                .antMatchers("/dashboard")
+                .authenticated();
+    }
 }
